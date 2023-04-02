c0976337d6f2e6271c9c3f3e6e4ad45e4d6c4b67
Ahmed Raza <ahmedraza6100@gmail.com>
2023-04-03 02:40:43 +0500

    tutorial completed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,65 @@
+// 'use client'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import React from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+async function getBlogs() {
+  const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=items`, { cache: 'no-store' });
 
-export default function Home() {
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function page() {
+  const blogs = await getBlogs();
+  console.log(blogs);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-gray-700 grid grid-cols-4 p-5 gap-5'>
+
+      {blogs.items.map((blog: any) => (
+        <div className=' bg-white p-5' key={blog.sys.id}>
+          {blogs.includes.Asset.map((elem: any) => (
+            <div key={blog.fields.image.sys.id}>
+              {blog.fields.image.sys.id == elem.sys.id ?
+                <Image src={"https:" + elem.fields.file.url} alt='' width={400} height={400} className='h-64' /> : <div></div>}
+            </div>
+          ))}
+
+          <h1 className=' text-3xl font-semibold p-2'>{blog.fields.title}</h1>
+          <p className='text-md'>{blog.fields.desc}</p>
+          <p className='text-md text-2xl'>Size: {blog.fields.size}</p>
+          <h2 className=' font-bold'>Rs: {blog.fields.price}</h2>
         </div>
+      ))}
+
+
+
+
+
+
+      {/* <div className=' bg-white p-5'>
+        <Image src={'/shirt.webp'} alt='' width={400} height={400} />
+        <h1 className=' text-3xl font-semibold p-2'>Male Shirt</h1>
+        <p className='text-md'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium esse officia libero explicabo laborum quod Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium esse officia libero</p>
+        <h2 className=' font-bold'>$50.00</h2>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+      <div className=' bg-white p-5'>
+        <Image src={'/shirt.webp'} alt='' width={400} height={400} />
+        <h1 className=' text-3xl font-semibold p-2'>Male Shirt</h1>
+        <p className='text-md'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium esse officia libero explicabo laborum quod Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium esse officia libero</p>
+        <h2 className=' font-bold'>$50.00</h2>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className=' bg-white p-5'>
+        <Image src={'/shirt.webp'} alt='' width={400} height={400} />
+        <h1 className=' text-3xl font-semibold p-2'>Male Shirt</h1>
+        <p className='text-md'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium esse officia libero explicabo laborum quod Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium esse officia libero</p>
+        <h2 className=' font-bold'>$50.00</h2>
+      </div> */}
+    </div>
   )
 }
